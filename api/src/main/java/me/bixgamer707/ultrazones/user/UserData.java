4feec20ce5f1ba72a2728ebaa7861fd432022fb4
bixgamer707 <bixgamer707@gmail.com>
2022-07-18 19:29:42 -0500

    feat saves, firstJoin and firstExit ] -> SAVES: Create method to loadData and saveData, USER: create firstJoin and starting create firstExit

--- api/src/main/java/me/bixgamer707/ultrazones/user/UserData.java
+++ api/src/main/java/me/bixgamer707/ultrazones/user/UserData.java
@@ -5,16 +5,20 @@ import me.bixgamer707.ultrazones.wgevents.events.RegionLeftEvent;
 import me.bixgamer707.ultrazones.wgevents.events.RegionsEnteredEvent;
 import me.bixgamer707.ultrazones.wgevents.events.RegionsLeftEvent;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public abstract class UserData {
 
     protected UUID uuid;
-    protected StringBuilder name;
+    protected ArrayList<String> zonesJoined;
+    protected String name;
+    protected ArrayList<String> zonesExit;
 
-    public UserData(UUID uuid, StringBuilder name){
+    public UserData(UUID uuid, String name){
         this.uuid = uuid;
         this.name = name;
+        this.zonesJoined = new ArrayList<>();
     }
     public abstract void onRegionJoin(RegionEnteredEvent event);
 
@@ -25,11 +29,35 @@ public abstract class UserData {
     public abstract void onRegionsLeft(RegionsLeftEvent event);
 
 
+    public ArrayList<String> getZonesJoined() {
+        return zonesJoined;
+    }
+
+    public void addZone(String id){
+        if(!zonesJoined.contains(id)){
+            zonesJoined.add(id);
+        }
+    }
+
+    public ArrayList<String> getZonesExit() {
+        return zonesExit;
+    }
+
+    public void addZoneExit(String id){
+        if(!zonesExit.contains(id)){
+            zonesExit.add(id);
+        }
+    }
+
+    protected abstract void loadData();
+
+    protected abstract void saveData();
+
     public UUID getUniqueId() {
         return uuid;
     }
 
-    public StringBuilder getName() {
+    public String getName() {
         return name;
     }
 
--- plugin/src/main/java/me/bixgamer707/ultrazones/user/User.java
+++ plugin/src/main/java/me/bixgamer707/ultrazones/user/User.java
@@ -14,7 +14,7 @@ import org.bukkit.Sound;
 import java.util.UUID;
 
 public class User extends UserData{
-    public User(UUID uuid, StringBuilder name) {
+    public User(UUID uuid, String name) {
         super(uuid, name);
     }
 
@@ -25,17 +25,33 @@ public class User extends UserData{
         File config = UltraZones.getInstance().getFileManager().getConfig();
         if(!config.contains("Zones"))return;
 
-        for(String zone : config.getConfigurationSection("Zones").getKeys(false)){
-            if(!config.contains("Zones."+zone+".join"))continue;
 
-            if(config.contains("Zones."+zone+".join.execute-commands")){
-                for(String cmd : config.getStringList("Zones."+zone+".join.execute-commands")){
+        String zone = event.getRegionName();
+        if(!config.contains("Zones."+zone+".join"))return;
+
+        if(config.contains("Zones."+zone+".join.permission-requirement")){
+            if(config.contains("Zones."+zone+".join.permission-requirement.value") &&
+                    config.getBoolean("Zones."+zone+".join.permission-requirement.enable")){
+                if(!event.getPlayer().hasPermission(config.getString("Zones."+zone+".join.permission-requirement.value"))){
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(Text.hexColors(config.getString("Zones."+zone+".join.permission-requirement.message-no-available")));
+                    return;
+                }
+            }
+        }
+
+        if(!getZonesJoined().contains(zone)){
+            addZone(zone);
+            if(!config.contains("Zones."+zone+".join.first-join"))return;
+
+            if(config.contains("Zones."+zone+".join.first-join.execute-commands")){
+                for(String cmd : config.getStringList("Zones."+zone+".join.first-join.execute-commands")){
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%",getName().toString()));
                 }
             }
-            if(config.contains("Zones."+zone+".join.sounds")){
-                if(config.contains("Zones."+zone+".join.sounds.to-server")){
-                    for(String sounds : config.getStringList("Zones."+zone+".join.sounds.to-server")){
+            if(config.contains("Zones."+zone+".join.first-join.sounds")){
+                if(config.contains("Zones."+zone+".join.first-join.sounds.to-server")){
+                    for(String sounds : config.getStringList("Zones."+zone+".join.first-join.sounds.to-server")){
                         String[] split = sounds.split(":");
                         try {
                             Sound sound = Sound.valueOf(split[0]);
@@ -52,8 +68,8 @@ public class User extends UserData{
                         }
                     }
                 }
-                if(config.contains("Zones."+zone+".join.sounds.player")){
-                    for(String sounds : config.getStringList("Zones."+zone+".join.sounds.player")){
+                if(config.contains("Zones."+zone+".join.first-join.sounds.player")){
+                    for(String sounds : config.getStringList("Zones."+zone+".join.first-join.sounds.player")){
                         String[] split = sounds.split(":");
                         try {
                             Sound sound = Sound.valueOf(split[0]);
@@ -69,24 +85,68 @@ public class User extends UserData{
                     }
                 }
             }
-            if(config.contains("Zones."+zone+".join.messages")){
-                if(config.contains("Zones."+zone+".join.messages.action-bar") &&
-                        config.getBoolean("Zones."+zone+".join.messages.action-bar.enable")){
-                    Text.sendActionBar(event.getPlayer(), config.getString("Zones."+zone+".join.messages.action-bar.message"));
+            if(config.contains("Zones."+zone+".join.first-join.messages")){
+                if(config.contains("Zones."+zone+".join.first-join.messages.action-bar") &&
+                        config.getBoolean("Zones."+zone+".join.first-join.messages.action-bar.enable")){
+                    Text.sendActionBar(event.getPlayer(), config.getString("Zones."+zone+".join.first-join.messages.action-bar.message"));
                 }
-                if(config.contains("Zones."+zone+".join.messages.message")){
+                if(config.contains("Zones."+zone+".join.first-join.messages.message")){
                     event.getPlayer().sendMessage(Text.sanitizeString(event.getPlayer(),
-                            config.getString("Zones."+zone+".join.messages.message")));
+                            config.getString("Zones."+zone+".join.first-join.messages.message")));
                 }
             }
-            if(config.contains("Zones."+zone+".join.permission-requirement")){
-                if(config.contains("Zones."+zone+".join.permission-requirement.value") &&
-                        config.getBoolean("Zones."+zone+".join.permission-requirement.enable")){
-                    if(!event.getPlayer().hasPermission(config.getString("Zones."+zone+".join.permission-requirement.value"))){
-                        event.setCancelled(true);
-                        event.getPlayer().sendMessage(Text.hexColors(config.getString("Zones."+zone+".join.permission-requirement.message-no-available")));
+            return;
+        }
+
+        if(config.contains("Zones."+zone+".join.execute-commands")){
+            for(String cmd : config.getStringList("Zones."+zone+".join.execute-commands")){
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%",getName().toString()));
+            }
+        }
+        if(config.contains("Zones."+zone+".join.sounds")){
+            if(config.contains("Zones."+zone+".join.sounds.to-server")){
+                for(String sounds : config.getStringList("Zones."+zone+".join.sounds.to-server")){
+                    String[] split = sounds.split(":");
+                    try {
+                        Sound sound = Sound.valueOf(split[0]);
+                        int vol = Integer.parseInt(split[1]);
+                        float pitch = Float.parseFloat(split[2]);
+
+                        Bukkit.getOnlinePlayers().forEach(player ->
+                                player.playSound(player.getLocation(),sound,vol,pitch)
+                        );
+                    }catch (IllegalArgumentException e){
+                        Text.sendMsgConsole(
+                                Color.RED+"THE SOUND "+ split[0] + "IS INCOMPATIBLE."
+                        );
                     }
                 }
+            }
+            if(config.contains("Zones."+zone+".join.sounds.player")){
+                for(String sounds : config.getStringList("Zones."+zone+".join.sounds.player")){
+                    String[] split = sounds.split(":");
+                    try {
+                        Sound sound = Sound.valueOf(split[0]);
+                        int vol = Integer.parseInt(split[1]);
+                        float pitch = Float.parseFloat(split[2]);
+
+                        event.getPlayer().playSound(event.getPlayer().getLocation(),sound,vol,pitch);
+                    }catch (IllegalArgumentException e){
+                        Text.sendMsgConsole(
+                                Color.RED+"SOUND "+ split[0] + "IS INCOMPATIBLE."
+                        );
+                    }
+                }
+            }
+        }
+        if(config.contains("Zones."+zone+".join.messages")){
+            if(config.contains("Zones."+zone+".join.messages.action-bar") &&
+                    config.getBoolean("Zones."+zone+".join.messages.action-bar.enable")){
+                Text.sendActionBar(event.getPlayer(), config.getString("Zones."+zone+".join.messages.action-bar.message"));
+            }
+            if(config.contains("Zones."+zone+".join.messages.message")){
+                event.getPlayer().sendMessage(Text.sanitizeString(event.getPlayer(),
+                        config.getString("Zones."+zone+".join.messages.message")));
             }
         }
     }
@@ -103,68 +163,70 @@ public class User extends UserData{
         File config = UltraZones.getInstance().getFileManager().getConfig();
         if(!config.contains("Zones"))return;
 
-        for(String zone : config.getConfigurationSection("Zones").getKeys(false)){
-            if(!config.contains("Zones."+zone+".left"))continue;
+        String zone = event.getRegionName();
 
-            if(config.contains("Zones."+zone+".left.execute-commands")){
-                for(String cmd : config.getStringList("Zones."+zone+".left.execute-commands")){
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%",getName().toString()));
+        if(!config.contains("Zones."+zone+".left"))return;
+
+        if(config.contains("Zones."+zone+".left.permission-requirement")){
+            if(config.contains("Zones."+zone+".left.permission-requirement.value") &&
+                    config.getBoolean("Zones."+zone+".left.permission-requirement.enable")){
+                if(!event.getPlayer().hasPermission(config.getString("Zones."+zone+".left.permission-requirement.value"))){
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(Text.hexColors(config.getString("Zones."+zone+".left .permission-requirement.message-no-available")));
+                    return;
                 }
             }
-            if(config.contains("Zones."+zone+".left.sounds")){
-                if(config.contains("Zones."+zone+".left.sounds.to-server")){
-                    for(String sounds : config.getStringList("Zones."+zone+".left.sounds.to-server")){
-                        String[] split = sounds.split(":");
-                        try {
-                            Sound sound = Sound.valueOf(split[0]);
-                            int vol = Integer.parseInt(split[1]);
-                            float pitch = Float.parseFloat(split[2]);
+        }
 
-                            Bukkit.getOnlinePlayers().forEach(player ->
-                                    player.playSound(player.getLocation(),sound,vol,pitch)
-                            );
-                        }catch (IllegalArgumentException e){
-                            Text.sendMsgConsole(
-                                    Color.RED+"THE SOUND "+ split[0] + "IS INCOMPATIBLE."
-                            );
-                        }
-                    }
-                }
-                if(config.contains("Zones."+zone+".left.sounds.player")){
-                    for(String sounds : config.getStringList("Zones."+zone+".left.sounds.player")){
-                        String[] split = sounds.split(":");
-                        try {
-                            Sound sound = Sound.valueOf(split[0]);
-                            int vol = Integer.parseInt(split[1]);
-                            float pitch = Float.parseFloat(split[2]);
+        if(config.contains("Zones."+zone+".left.execute-commands")){
+            for(String cmd : config.getStringList("Zones."+zone+".left.execute-commands")){
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%",getName().toString()));
+            }
+        }
+        if(config.contains("Zones."+zone+".left.sounds")){
+            if(config.contains("Zones."+zone+".left.sounds.to-server")){
+                for(String sounds : config.getStringList("Zones."+zone+".left.sounds.to-server")){
+                    String[] split = sounds.split(":");
+                    try {
+                        Sound sound = Sound.valueOf(split[0]);
+                        int vol = Integer.parseInt(split[1]);
+                        float pitch = Float.parseFloat(split[2]);
 
-                            event.getPlayer().playSound(event.getPlayer().getLocation(),sound,vol,pitch);
-                        }catch (IllegalArgumentException e){
-                            Text.sendMsgConsole(
-                                    Color.RED+"THE SOUND "+ split[0] + "IS INCOMPATIBLE."
-                            );
-                        }
+                        Bukkit.getOnlinePlayers().forEach(player ->
+                                player.playSound(player.getLocation(),sound,vol,pitch)
+                        );
+                    }catch (IllegalArgumentException e){
+                        Text.sendMsgConsole(
+                                Color.RED+"THE SOUND "+ split[0] + "IS INCOMPATIBLE."
+                        );
                     }
                 }
             }
-            if(config.contains("Zones."+zone+".left.messages")){
-                if(config.contains("Zones."+zone+".left.messages.action-bar") &&
-                        config.getBoolean("Zones."+zone+".left.messages.action-bar.enable")){
-                    Text.sendActionBar(event.getPlayer(), config.getString("Zones."+zone+".left.messages.action-bar.message"));
-                }
-                if(config.contains("Zones."+zone+".left.messages.message")){
-                    event.getPlayer().sendMessage(Text.sanitizeString(event.getPlayer(),
-                            config.getString("Zones."+zone+".left.messages.message")));
-                }
-            }
-            if(config.contains("Zones."+zone+".left.permission-requirement")){
-                if(config.contains("Zones."+zone+".left.permission-requirement.value") &&
-                        config.getBoolean("Zones."+zone+".left.permission-requirement.enable")){
-                    if(!event.getPlayer().hasPermission(config.getString("Zones."+zone+".left.permission-requirement.value"))){
-                        event.setCancelled(true);
-                        event.getPlayer().sendMessage(Text.hexColors(config.getString("Zones."+zone+".left .permission-requirement.message-no-available")));
+            if(config.contains("Zones."+zone+".left.sounds.player")){
+                for(String sounds : config.getStringList("Zones."+zone+".left.sounds.player")){
+                    String[] split = sounds.split(":");
+                    try {
+                        Sound sound = Sound.valueOf(split[0]);
+                        int vol = Integer.parseInt(split[1]);
+                        float pitch = Float.parseFloat(split[2]);
+
+                        event.getPlayer().playSound(event.getPlayer().getLocation(),sound,vol,pitch);
+                    }catch (IllegalArgumentException e){
+                        Text.sendMsgConsole(
+                                Color.RED+"THE SOUND "+ split[0] + "IS INCOMPATIBLE."
+                        );
                     }
                 }
+            }
+        }
+        if(config.contains("Zones."+zone+".left.messages")){
+            if(config.contains("Zones."+zone+".left.messages.action-bar") &&
+                    config.getBoolean("Zones."+zone+".left.messages.action-bar.enable")){
+                Text.sendActionBar(event.getPlayer(), config.getString("Zones."+zone+".left.messages.action-bar.message"));
+            }
+            if(config.contains("Zones."+zone+".left.messages.message")){
+                event.getPlayer().sendMessage(Text.sanitizeString(event.getPlayer(),
+                        config.getString("Zones."+zone+".left.messages.message")));
             }
         }
     }
