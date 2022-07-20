23a1c55ab627afdd81ec7c41714d538c894f2e69
bixgamer707 <bixgamer707@gmail.com>
2022-07-20 11:48:15 -0500

    feat plugin ] -> Now the sv version is obtained from the required method itself.

--- plugin/src/main/java/me/bixgamer707/ultrazones/UltraZones.java
+++ plugin/src/main/java/me/bixgamer707/ultrazones/UltraZones.java
@@ -3,63 +3,78 @@ package me.bixgamer707.ultrazones;
 import me.bixgamer707.ultrazones.manager.*;
 import me.bixgamer707.ultrazones.register.RegisterPlugin;
 import me.bixgamer707.ultrazones.utils.Text;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UltraZones extends JavaPlugin {
 
     private static UltraZones instance;
     private FileManager fileManager;
-    public String nmsVer;
-    private boolean useOldMethods = false;
     private RegisterPlugin registerPlugin;
     private UsersManager usersManager;
     private WorldGuardManager wgManager;
     @Override
     public void onEnable(){
-        //REGISTER NMS VERSION
-        nmsVer = Bukkit.getServer().getClass().getPackage().getName();
-        nmsVer = nmsVer.substring(nmsVer.lastIndexOf(".") + 1);
-        if (nmsVer.equalsIgnoreCase("v1_8_R1") || nmsVer.startsWith("v1_7_")) {
-            useOldMethods = true;
-        }
-
         // CREATING INSTANCES
-        long currentMs = System.currentTimeMillis();
         instance = this;
-        this.fileManager = new FileManager(this);
+        long ms = System.currentTimeMillis();
         this.registerPlugin = new RegisterPlugin(this);
-        this.usersManager = new UsersManager();
+        this.fileManager = new FileManager(this);
         this.wgManager = new WorldGuardManager(this);
+        this.usersManager = new UsersManager();
 
         //REGISTER FUNCTIONS
-        registerPlugin.registerAll();
-        fileManager.registerFiles();
-        wgManager.startWG();
+        loader(
+                this.fileManager,
+                this.usersManager,
+                this.wgManager,
+                this.registerPlugin
+        );
+
+
         new Metrics(this, 15809);
         new UpdateChecker(this, 103295).getVersion(version -> {
             if (this.getDescription().getVersion().equals(version)) {
                 getLogger().info(Text.hexColors("&aThere is not a new update available."));
             } else {
-                getLogger().info(Text.hexColors("&6There is a new update available."));
-                getLogger().info(Text.hexColors("&9You can download in: "));
-                getLogger().info(Text.hexColors("&bhttps://www.spigotmc.org/resources/ultrazones.103295/"));
+                Text.sendMsgConsole(
+                        "&6There is a new update available.",
+                        "&9You can download in: ",
+                        "&bhttps://www.spigotmc.org/resources/ultrazones.103295/"
+                );
             }
         });
-        getLogger().info(Text.hexColors("&fLOAD PLUGIN IN: &a"+ (System.currentTimeMillis() - currentMs) + "ms"));
+        Text.sendMsgConsole(
+                "&fLOAD PLUGIN IN: &a"+ (System.currentTimeMillis() - ms) + "ms"
+        );
     }
 
     @Override
     public void onDisable(){
-        wgManager.stop();
+        long ms = System.currentTimeMillis();
+        unLoader(
+                this.registerPlugin,
+                this.usersManager,
+                this.fileManager,
+                this.wgManager
+        );
+        Text.sendMsgConsole(
+                "&fUN LOAD PLUGIN IN: &6"+ (System.currentTimeMillis() - ms) + "ms"
+        );
         instance = null;
-        wgManager = null;
-        fileManager = null;
-        registerPlugin = null;
-        usersManager = null;
-        nmsVer = null;
     }
 
+
+    public void loader(LoaderManager... loaderManagers){
+        for(LoaderManager loaderManager : loaderManagers){
+            loaderManager.start();
+        }
+    }
+
+    public void unLoader(LoaderManager... loaderManagers){
+        for(LoaderManager loaderManager : loaderManagers){
+            loaderManager.stop();
+        }
+    }
 
     public static UltraZones getInstance() {
         return instance;
@@ -67,14 +82,6 @@ public class UltraZones extends JavaPlugin {
 
     public FileManager getFileManager() {
         return this.fileManager;
-    }
-
-    public boolean isUseOldMethods() {
-        return useOldMethods;
-    }
-
-    public String getNmsVer() {
-        return nmsVer;
     }
 
     public UsersManager getUsersManager() {
